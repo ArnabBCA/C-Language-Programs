5a7cc83dc35df8c7a6383ea06351b8359ff20862
Arnab Ghosh <arnabsonu2@gmail.com>
2022-10-23 21:14:21 +0530

    Parenthisis Matching

--- Array Programs/Parenthisis_Matching.c
+++ Array Programs/Parenthisis_Matching.c
@@ -5,29 +5,43 @@ int main()
 { 
     int top=-1;
     int count=0;
+    int del=0;
     int i;
     int j;
     char ex[size];
     char arr[size];
     printf("Enter the expression");
     scanf("%s",&ex);
-    //printf("%s",ex);
-    for(i=0;i<(strlen(ex));i++)
+    int length=strlen(ex);
+    for(i=0;i<length;i++)
     {
-        //printf("%c\n",ex[i]);
         if(ex[i]=='(')
         {
             count=count+1;
             top=top+1;
 	        arr[top]=ex[i];
-            printf("%c",ex[i]);
         }
-
+        else if(ex[i]==')')
+        {
+            top=top-1;
+        }
+        else
+        {
+            continue;
+        }          
     }
-    for(j=0;j<strlen(arr);j++)
+    if(top==0)
+    {
+        printf("Your expression is ok");
+    }
+    else
+    {
+        printf("Wrong Expression")
+    }
+
+    for(j=0;j<top-1;j++)
     {
         printf("%c",arr[j]);
     }
-    printf("%d\n",strlen(arr));
     return 0;
 }
